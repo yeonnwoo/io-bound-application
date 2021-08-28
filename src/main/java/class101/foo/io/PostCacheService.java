1b23cbd9260346f8c71dcd4e67bff24b4454b2a9
Yeonwoo-Kim <yeonkim0726@gmail.com>
2021-08-28 10:44:15 +0900

    feat: 글 목록 캐싱, rabbitMQ 추가

---
+++ src/main/java/class101/foo/io/PostCacheService.java
@@ -0,0 +1,30 @@
+package class101.foo.io;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostCacheService {
+
+    @Autowired
+    PostRepository postRepository;
+
+    private Page<Post> firstPostPage;
+
+    //1초에 한번씩 실행
+    @Scheduled(cron = "* * * * * *")
+    public void updateFirstPostPage() {
+        firstPostPage = postRepository.findAll(
+            PageRequest.of(0, 20, Sort.by("id").descending()));
+    }
+
+    public Page<Post> getFirstPostPage()
+    {
+        return this.firstPostPage;
+    }
+
+}
